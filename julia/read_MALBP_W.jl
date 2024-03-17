6fefbcff276739a5dad4a6ad2f4bdfbaa35636c9
Joseph Thompson <joseph.thompson@polymtl.ca>
2024-03-17 11:13:35 -0400

    moved model instance generation

--- julia/read_MALBP_W.jl
+++ julia/read_MALBP_W.jl
@@ -1,11 +1,14 @@
 using CSV
 using DataFrames
 import YAML
+include("scenario_generators.jl")
 
-#reads scenario tree file from csv
-function read_scenario_tree(scenario_info::Dict)
+#reads scenario tree file from csv accepts a dictionary of scenario tree info and model mixtures(optional)
+function read_scenario_tree(scenario_info::Dict, model_mixtures::Dict{String, Float64} )
     if scenario_info["generator"] == "read_csv"
         return CSV.read(scenario_info["filepath"], DataFrame)
+    elseif scenario_info["generator"] == "full"
+        return generate_scenario_tree(scenario_info["sequence_length"], model_mixtures)
     else
         error("unrecognized generator, currently we only support read_csv for scenario tree generation.")
     end
@@ -43,6 +46,7 @@ struct MALBP_W_instance
     config_name::String
     models::ModelsInstance
     scenarios::DataFrame
+    no_scenarios::Int
     equipment::EquipmentInstance
     no_stations:: Int
     max_workers:: Int
@@ -51,6 +55,15 @@ struct MALBP_W_instance
     sequence_length:: Int
     no_cycles:: Int
     MILP_models::Array{String}
+end
+
+function calculate_scenarions(scenarios::DataFrame)
+    return nrow(scenarios)
+end
+
+function MALBP_W_instance(config_name::String, models::ModelsInstance, scenarios::DataFrame, equipment::EquipmentInstance, no_stations:: Int, max_workers:: Int, worker_cost:: Int, recourse_cost:: Int, sequence_length:: Int, no_cycles:: Int, MILP_models::Array{String})
+    no_scenarios = calculate_scenarions(scenarios)
+    return MALBP_W_instance(config_name, models, scenarios,no_scenarios, equipment, no_stations, max_workers, worker_cost, recourse_cost, sequence_length, no_cycles, MILP_models)
 end
 
 
@@ -86,7 +99,12 @@ function read_models_instance(file_name :: String)
     models_yaml = YAML.load(open(file_name))
     models = []
     instance_name = models_yaml["name"]
-    cycle_time = models_yaml["takt_time"]
+    #if the cycle time is defined as "takt_time" in the yaml file, read instance
+    if haskey(models_yaml, "takt_time")
+        cycle_time = models_yaml["takt_time"]
+    else
+        cycle_time = models_yaml["cycle_time"]
+    end
     #reads the model instances
     for (key, value) in models_yaml["model_data"]
         name = key
@@ -104,18 +122,41 @@ function read_models_instance(file_name :: String)
     return models_instance
 end
 
+#Gets the model mixture from the models instance. The model mixture is a dictionary with model name as
+# key and probability as value
+function get_model_mixture(models_instance::ModelsInstance)
+    model_mixture = Dict{String, Float64}()
+    for model in models_instance.models
+        model_mixture[model.name] = model.probability
+    end
+    return model_mixture
+end
 
+#checks the instance for consistency
+function check_instance(config_file, models_instance, equipment_instance)
+    if config_file["no_stations"] != equipment_instance.no_stations
+        error("number of stations in the config file does not match the number of stations in the equipment instance")
+    end
+    #If the tasks in the models instance are not in the equipment instance, throw an error
+    for model in models_instance.models
+        for task in model.task_times
+            if task[1] > equipment_instance.no_tasks
+                error("task $(task[1]) in model $(model.name) is not in the equipment instance")
+            end
+        end
+    end
+end
 #takes an instance filepath as an input, and returns an array of MALBP_W_instance struct
 function read_MALBP_W_instances(file_name::String)
     config_file = get_instance_YAML(file_name)
     instances = []
-    println(config_file)
     for model in config_file["model_files"]
         for equip in config_file["equipment_files"]
             models_instance = read_models_instance(model)
             equipment_instance = read_equipment_instance(equip)
-            scenarios = read_scenario_tree(config_file["scenario_generator"])
-            no_cycles = config_file["sequence_length"] + config_file["no_stations"] - 1
+            check_instance(config_file,models_instance, equipment_instance)
+            scenarios = read_scenario_tree(config_file["scenario"], get_model_mixture(models_instance))
+            no_cycles = config_file["scenario"]["sequence_length"] + config_file["no_stations"] - 1
             current_instance =MALBP_W_instance(config_file["config_name"], 
                         models_instance, 
                         scenarios, 
@@ -124,7 +165,7 @@ function read_MALBP_W_instances(file_name::String)
                         config_file["max_workers"], 
                         config_file["worker_cost"], 
                         config_file["recourse_cost"], 
-                        config_file["sequence_length"],
+                        config_file["scenario"]["sequence_length"],
                         no_cycles, 
                         config_file["milp_models"])
             push!(instances, current_instance)
@@ -143,4 +184,4 @@ end
 
 # tree = read_scenario_tree("SALBP_benchmark/MM_instances/scenario_trees/5_takts_5_samples_3_models.csv")
 # print_scenario_tree(tree)
-instances = read_MALBP_W_instances("SALBP_benchmark/MM_instances/julia_debug.yaml")
+#instances = read_MALBP_W_instances("SALBP_benchmark/MM_instances/julia_debug.yaml")
